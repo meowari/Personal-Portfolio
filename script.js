ff3f4d39910f15badc8268a688580629b82b7f6c
annemy0 <annemy002@gmail.com>
2023-12-06 22:37:44 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//home page//
+function myFunction() {
+    window.location.href = "contact.html";
+  }
+
+//works page//
+  function myGithub(){
+    window.location.href ="https://github.com/meowari"
+  }
